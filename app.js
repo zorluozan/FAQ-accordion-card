85ac904fd97ef824e376bbd354a3fac5bb9e6718
zorluozan <61868614+zorluozan@users.noreply.github.com>
2022-02-02 14:30:30 +0300

    Readme added and some css issues solved

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let icon_button = document.getElementsByClassName("card__content__header");
 
-for(let i=0; i<icon_button.length;i++) {
-    icon_button[i].onclick = function() {
-        const setClasses = !this.classList.contains("active");
-        setClass(icon_button,"active","remove");
+for (let i = 0; i < icon_button.length; i++) {
+  icon_button[i].onclick = function () {
+    const setClasses = !this.classList.contains("active");
+    setClass(icon_button, "active", "remove");
 
-        if(setClasses) {
-           this.classList.toggle("active");
-        }
+    if (setClasses) {
+      this.classList.toggle("active");
     }
+  };
 }
 
 function setClass(elements, className, functionName) {
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList[functionName](className);
-    }
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList[functionName](className);
+  }
 }
